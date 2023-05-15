10a8635470db8d4776c2581268f7829780e42630
gokarna123-goku <agokarna444@gmail.com>
2023-05-15 12:58:01 +0545

    worked on navbar scroll section

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
-// 
+//
+window.addEventListener("scroll", function () {
+  var navbar = document.querySelector("nav");
+  var scrollY = window.scrollY;
 
+  if (scrollY >= 50) {
+    navbar.classList.add("fixed");
+  } else {
+    navbar.classList.remove("fixed");
+  }
+});
 
-
-// 
+//
